190a964f5a97b7418bf77271e259d802b6cf262d
hweem <hweemyoung@gmail.com>
2019-11-19 17:56:54 +0900

    js function object

---
+++ WEB1-html/js/color.js
@@ -0,0 +1,9 @@
+var BODY = {
+    body_style: document.querySelector('body').style,
+    setBGColor: function setBGColor(color) {
+    this.body_style.backgroundColor = color;
+},
+    setColor: function setColor(color) {
+        this.body_style.color = color
+    }
+}
